f7c8510a870658ee7cb3166279285a1dfb0c8fbc
Nik <novakn@mcmaster.ca>
2026-03-13 13:54:24 -0700

    keep routes version-controlled and immuteable

--- src/app/api/v2/version/route.ts
+++ src/app/api/v2/version/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import constants from '../../../../CONSTANTS.json';
+import constants from '../../../../../CONSTANTS.json';
 
 export async function GET(request: Request) {
   return new Response(constants.version)

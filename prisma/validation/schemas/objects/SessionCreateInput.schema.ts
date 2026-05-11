73cf7dca87d9b4ab7dfe1b876a893f9b9fe4e78e
Nik <novakn@mcmaster.ca>
2026-05-10 18:20:16 -0700

    4.2.0 synchronize schema

--- prisma/validation/schemas/objects/SessionCreateInput.schema.ts
+++ prisma/validation/schemas/objects/SessionCreateInput.schema.ts
@@ -3,7 +3,7 @@ import type { Prisma } from '@prisma/client';
 import { UserCreateNestedOneWithoutSessionsInputObjectSchema as UserCreateNestedOneWithoutSessionsInputObjectSchema } from './UserCreateNestedOneWithoutSessionsInput.schema'
 
 const makeSchema = () => z.object({
-  id: z.string().max(24),
+  id: z.string().max(24).optional(),
   token: z.string(),
   expiresAt: z.coerce.date(),
   ipAddress: z.string().optional().nullable(),
--- prisma/validation/schemas/objects/SessionUncheckedCreateWithoutUserInput.schema.ts
+++ prisma/validation/schemas/objects/SessionUncheckedCreateWithoutUserInput.schema.ts
@@ -3,7 +3,7 @@ import type { Prisma } from '@prisma/client';
 
 
 const makeSchema = () => z.object({
-  id: z.string(),
+  id: z.string().optional(),
   token: z.string(),
   expiresAt: z.coerce.date(),
   ipAddress: z.string().optional().nullable(),
